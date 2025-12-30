a387c8b75fa5edb5f19f99196787260e70dc53b5
BrayanDevMode <bcarlosac20@gmail.com>
2025-12-29 23:39:01 -0500

    Fist commit, starting the components

---
+++ bomber-backend/src/main/java/com/brayan/bomber_backend/BomberBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brayan.bomber_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomberBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomberBackendApplication.class, args);
+	}
+
+}
---
+++ bomber-backend/src/test/java/com/brayan/bomber_backend/BomberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brayan.bomber_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomberBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
